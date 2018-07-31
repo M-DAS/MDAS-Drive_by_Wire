f0caeafc3a26861d597620040c30ff23e4ded600
Ben DiDonato <bddidona@umich.edu>
2018-07-30 20:40:39 -0400

    Reupload Pedal_ADC.c

--- Throttle/Pedal_ADC.c
+++ Throttle/Pedal_ADC.c
@@ -12,7 +12,7 @@
 
 
 //Function to read the throttle input
-uint32_t get_throttle_input(void)
+uint32_t get_throttle_input_low(void)
 {
 	uint32_t ADC_value;
 	ADCIntClear(ADC0_BASE, 1);
@@ -25,6 +25,20 @@ uint32_t get_throttle_input(void)
 	
 	//Get the ADC value
 	ADCSequenceDataGet(ADC0_BASE, 1, &ADC_value);
+	return ADC_value;
+}
+
+uint32_t get_throttle_input_high(void)
+{
+	uint32_t ADC_value;
+	//Start conversion sequence
+	ADCProcessorTrigger(ADC1_BASE, 1);
+	while(!ADCIntStatus(ADC1_BASE, 1, false))
+	{
+	}
+	
+	//Get the ADC value
+	ADCSequenceDataGet(ADC1_BASE, 1, &ADC_value);
 	return ADC_value;
 }
 
@@ -45,9 +59,31 @@ void ADC0_Setup(void)
 	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))
 	{}
 	
-	//Setup ADC0 sequencer 1 to read the throttle pedal input
+	//Setup ADC0 sequencer 1 to read the throttle pedal input high
 	ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
 	ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_CH4|ADC_CTL_IE|ADC_CTL_END);
 	ADCSequenceEnable(ADC0_BASE, 1);
 }
+
+void ADC1_Setup(void)
+{
+	//Enable PORTB
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+	{}
+
+	//Configure PORTB pin 5 for analog input
+	//PB5 pedal input CH11
+	GPIOPinTypeADC(GPIO_PORTB_BASE, GPIO_PIN_5);
+		
+	//Enable ADC1
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC1))
+	{}
+	
+	//Setup ADC1 sequencer 1 to read the throttle pedal input low
+	ADCSequenceConfigure(ADC1_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
+	ADCSequenceStepConfigure(ADC1_BASE, 1, 0, ADC_CTL_CH11|ADC_CTL_IE|ADC_CTL_END);
+	ADCSequenceEnable(ADC1_BASE, 1);
+	}
 
